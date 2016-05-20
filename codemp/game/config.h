8d89060d2ef3b490245250a662f78f83b10e16f1
roque <roque@webdevel.pl>
2016-05-21 00:30:18 +0200

    Config Update for new vars [SERVER_NAME]

--- codemp/game/config.h
+++ codemp/game/config.h
@@ -1,2 +1,5 @@
-#define	CONFIG_GAMENAME "OpenJK"
-#define	CONFIG_GAMENAME_LOWER "openjk"
+#define	CONFIG_GAMENAME 		"OpenJK"
+#define	CONFIG_GAMENAME_LOWER 	"openjk"
+
+#define CONFIG_UPDATE_SERVER_NAME			"updatejk3.ravensoft.com"
+#define CONFIG_MASTER_SERVER_NAME			"masterjk3.ravensoft.com"
