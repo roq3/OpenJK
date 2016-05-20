48642b9c76121043aa3b3fa44b088932201fd40b
roque <roque@webdevel.pl>
2016-05-21 00:29:15 +0200

    Change name of JK_VERSION_OLD to new one

--- codemp/qcommon/game_version.h
+++ codemp/qcommon/game_version.h
@@ -36,8 +36,8 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 #if defined(_DEBUG)
 	#define	JK_VERSION		"(debug)"CONFIG_GAMENAME"-MP: v" VERSION_STRING_DOTTED
-	#define JK_VERSION_OLD	"(debug)JAmp: v" VERSION_STRING_DOTTED
+	#define	JK_VERSION_OLD	"(debug)"CONFIG_GAMENAME"-MP: v" VERSION_STRING_DOTTED
 #else
 	#define	JK_VERSION		CONFIG_GAMENAME"-MP: v" VERSION_STRING_DOTTED
-	#define JK_VERSION_OLD	"JAmp: v" VERSION_STRING_DOTTED
+	#define	JK_VERSION_OLD	CONFIG_GAMENAME"-MP: v" VERSION_STRING_DOTTED
 #endif
